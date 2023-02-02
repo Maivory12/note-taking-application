ba79e4d57485b4611af43ce853e375a34f1ad80e
Marques Ivory <Maivory12@icloud.com>
2023-02-01 19:39:06 -0500

    Added middleware for parsing JSON

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const fs = require(`fs`);
 const notes = require (`./db/db.json`)
 const app = express()
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 
 
 // Tell the server to listen for request
